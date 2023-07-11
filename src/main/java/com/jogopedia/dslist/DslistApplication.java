b8c464e7cbf675cfcc01543c295dff96d126a168
Lucas Davi <lucasdavi99@hotmail.com>
2023-07-11 02:29:29 -0300

    Properties, Game, ORM

--- src/main/java/com/jogopedia/dslist/DslistApplication.java
+++ src/main/java/com/jogopedia/dslist/DslistApplication.java
@@ -2,8 +2,10 @@ package com.jogopedia.dslist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.jogopedi.dslist.entities")
 public class DslistApplication {
 
 	public static void main(String[] args) {
